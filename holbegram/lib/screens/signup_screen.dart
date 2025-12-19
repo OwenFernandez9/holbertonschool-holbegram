b1c546ebef177480c41f2cdbc61a12c22836b88e
OwenFernandez9 <9357@holbertonstudents.com>
2025-12-19 16:39:10 -0300

    Add sign up screen

--- holbegram/lib/screens/signup_screen.dart
+++ holbegram/lib/screens/signup_screen.dart
@@ -1,58 +1,202 @@
 import 'package:flutter/material.dart';
 import '../widgets/text_field.dart';
+import 'login_screen.dart';
 
-class SignupScreen extends StatefulWidget {
-  const SignupScreen({super.key});
+class SignUp extends StatefulWidget {
+  final TextEditingController emailController;
+  final TextEditingController usernameController;
+  final TextEditingController passwordController;
+  final TextEditingController passwordConfirmController;
+
+  final bool _passwordVisible;
+
+  const SignUp({
+    super.key,
+    required this.emailController,
+    required this.usernameController,
+    required this.passwordController,
+    required this.passwordConfirmController,
+    bool passwordVisible = true,
+  }) : _passwordVisible = passwordVisible;
 
   @override
-  State<SignupScreen> createState() => _SignupScreenState();
+  State<SignUp> createState() => _SignUpState();
 }
 
-class _SignupScreenState extends State<SignupScreen> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
+class _SignUpState extends State<SignUp> {
+  late bool _passwordVisible;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = widget._passwordVisible;
+  }
 
   @override
   void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    _confirmPasswordController.dispose();
+    widget.emailController.dispose();
+    widget.usernameController.dispose();
+    widget.passwordController.dispose();
+    widget.passwordConfirmController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up')),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextField(
-              controller: _emailController,
-              hintText: 'Email',
-            ),
-            const SizedBox(height: 12),
-            CustomTextField(
-              controller: _passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-            const SizedBox(height: 12),
-            CustomTextField(
-              controller: _confirmPasswordController,
-              hintText: 'Confirm Password',
-              obscureText: true,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Create Account'),
-            ),
-          ],
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 28),
+                    const Text(
+                      'Holbegram',
+                      style: TextStyle(
+                        fontFamily: 'Billabong',
+                        fontSize: 50,
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/images/logo.webp',
+                      width: 80,
+                      height: 60,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 12),
+                          const Text(
+                            'Sign up to see photos and videos\nfrom your friends.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(height: 28),
+
+                          TextFieldInput(
+                            controller: widget.emailController,
+                            ispassword: false,
+                            hintText: 'Email',
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          const SizedBox(height: 24),
+
+                          TextFieldInput(
+                            controller: widget.usernameController,
+                            ispassword: false,
+                            hintText: 'Full Name',
+                            keyboardType: TextInputType.text,
+                          ),
+                          const SizedBox(height: 24),
+
+                          TextFieldInput(
+                            controller: widget.passwordController,
+                            ispassword: !_passwordVisible,
+                            hintText: 'Password',
+                            keyboardType: TextInputType.visiblePassword,
+                            suffixIcon: IconButton(
+                              alignment: Alignment.bottomLeft,
+                              onPressed: () {
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                              icon: Icon(
+                                _passwordVisible
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: const Color.fromARGB(218, 226, 37, 24),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+
+                          TextFieldInput(
+                            controller: widget.passwordConfirmController,
+                            ispassword: !_passwordVisible,
+                            hintText: 'Confirm Password',
+                            keyboardType: TextInputType.visiblePassword,
+                            suffixIcon: IconButton(
+                              alignment: Alignment.bottomLeft,
+                              onPressed: () {
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                              icon: Icon(
+                                _passwordVisible
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: const Color.fromARGB(218, 226, 37, 24),
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 28),
+
+                          SizedBox(
+                            height: 48,
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor: WidgetStateProperty.all(
+                                  const Color.fromARGB(218, 226, 37, 24),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                'Sign up',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(height: 24),
+                          const Divider(thickness: 2),
+
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Have an account?'),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => LoginScreen(
+                                          emailController: TextEditingController(),
+                                          passwordController: TextEditingController(),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: const Text(
+                                    'Log in',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromARGB(218, 226, 37, 24),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
